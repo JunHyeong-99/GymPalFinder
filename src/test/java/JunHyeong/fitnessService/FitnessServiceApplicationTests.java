6ad46be6c04833467c1c521a6056b216cd24a281
junhyeong Park <64734115+pnu201924480@users.noreply.github.com>
2023-11-20 13:47:03 +0900

    init project

---
+++ src/test/java/JunHyeong/fitnessService/FitnessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package JunHyeong.fitnessService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
